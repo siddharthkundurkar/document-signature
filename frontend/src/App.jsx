706b7e6b0bfec0ae95dd1a14b56504c9bf3e9866
Siddharth Kundurkar <siddharthkundurkar@gmail.com>
2026-06-11 13:34:42 +0530

    Day 1 Task Done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import './index.css';
+
+function App()
+{  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+      Document Signature App
+      </h1>
+    </div>
+  )
+}
+
+export default App
